65cf4f9b1cc498e5945dd26d8282af09aeb37755
comejia <comejia@teco.com.ar>
2025-10-30 17:57:43 -0300

    Example: Google drive loader

---
+++ src/examples/rag/document_loaders.py
@@ -0,0 +1,17 @@
+from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader
+
+loader = PyPDFLoader("/home/comejia/projects/langchain-project/src/examples/rag/Profile.pdf")
+
+pages = loader.load()
+
+for i, page in enumerate(pages):
+    print(f"=== Pagina {i+1} ===")
+    print(f"Contenido: {page.page_content}")
+    print(f"Metadatos: {page.metadata}")
+
+
+loader = WebBaseLoader("https://techmind.ac/")
+
+data = loader.load()
+
+print(data)
---
+++ src/examples/rag/google_drive_loader.py
@@ -0,0 +1,16 @@
+from langchain_google_community import GoogleDriveLoader
+
+credentials_path = "/home/comejia/projects/langchain-project/src/examples/rag/credentials.json"
+token_path = "/home/comejia/projects/langchain-project/src/examples/rag/token.json"
+
+loader = GoogleDriveLoader(
+    folder_id="1TOqon2698K2FOHY2dQE8XgyAxXDSDPMn",
+    credentials_path=credentials_path,
+    token_path=token_path,
+    recursive=True,
+)
+
+documents = loader.load()
+
+print(f"Metadata: {documents[0].metadata}")
+print(f"Contenido: {documents[0].page_content}")
